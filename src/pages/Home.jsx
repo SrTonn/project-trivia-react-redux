d51fc1701c6eaae8a6a67416c79b7a4ebb2ded5d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-09 16:42:17 -0300

    pagina Home criada

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Form from '../components/Form/Form';
+import logo from '../trivia.png';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <Form />
+        </header>
+      </div>
+    );
+  }
+}
