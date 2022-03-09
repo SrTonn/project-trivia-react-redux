a6d3f268e25f9406547618f3e1f521f3e6422772
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-09 17:17:26 -0300

    provider adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { BrowserRouter, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Provider } from 'react-redux';
 import Home from './pages/Home';
 import './App.css';
+import store from './redux/store';
 
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Home } />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={ store }>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
