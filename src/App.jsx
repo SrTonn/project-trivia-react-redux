4efae46973be5d25fb5a5562d0bf0ba3a28f8458
Thiaracosta <thiaramscosta@gmail.com>
2022-03-09 23:37:22 -0300

    criando rota para Settings

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import './App.css';
 import store from './redux/store';
 import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default class App extends React.Component {
   render() {
@@ -15,6 +16,7 @@ export default class App extends React.Component {
           <Switch>
             <Route exact path="/" component={ Home } />
             <Route path="/game" component={ Game } />
+            <Route path="/settings" component={ Settings } />
           </Switch>
         </BrowserRouter>
       </Provider>
