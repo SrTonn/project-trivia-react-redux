3ee750c134aac79ab9057fea5d4f27f9889966a5
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-14 15:39:23 -0300

    requisito 13 concluido

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header/Header';
 
-export default class Settings extends Component {
+class Settings extends Component {
   render() {
+    const { assertions } = this.props;
+    const NUMBER_THREE = 3;
+
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text">...</p>
+        <p data-testid="feedback-text">
+          {assertions < NUMBER_THREE ? 'Could be better...' : 'Well Done!'}
+        </p>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ player }) => ({
+  ...player,
+});
+
+export default connect(mapStateToProps)(Settings);
+
+Settings.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
